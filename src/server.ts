742160c05e6b2262ca81050da37cf608398d5014
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-10-17 21:32:29 -0300

    Configurando o swagger

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import swaggerUI from 'swagger-ui-express';
 import { routes } from './routes';
+import swaggerFile from './swagger.json';
 
 const app = express();
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(express.json());
 
