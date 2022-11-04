c43dac1dad2d0f92fae58d1021dac7db53bfe298
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-11-04 14:40:49 -0300

    Criptografar senha

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcryptjs';
 import { inject, injectable } from 'tsyringe';
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
@@ -10,7 +11,19 @@ class CreateUserUseCase {
   ) {}
 
   async execute({ name, email, driver_license, password }: ICreateUserDTO): Promise<void> {
-    await this.usersRepository.create({ name, email, driver_license, password });
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists.');
+    }
+
+    const passwordHash = await hash(password, 8);
+    await this.usersRepository.create({
+      name,
+      email,
+      driver_license,
+      password: passwordHash
+    });
   }
 }
 
