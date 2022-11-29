043201d115f57696a106102c97cc63cb32d242c4
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-11-28 22:45:32 -0300

    Criando seed de usuário

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,12 +3,14 @@ import { authenticateRoutes } from './authenticate.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { usersRoutes } from './users.routes';
+import { carsRoutes } from './cars.routes';
 
 const routes = Router();
 
 routes.use('/categories', categoriesRoutes);
 routes.use('/specifications', specificationsRoutes);
 routes.use('/users', usersRoutes);
+routes.use('/cars', carsRoutes);
 routes.use(authenticateRoutes);
 
 export { routes };
