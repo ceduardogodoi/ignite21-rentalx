93bc8718afe62e8f53f6cfcd2836188bb22a4806
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-12-28 08:59:02 -0300

    Configurando supertest

--- src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -1,13 +1,13 @@
 import { inject, injectable } from 'tsyringe';
-import { Category } from '@modules/cars/entities/Category';
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
+import { Category } from '@modules/cars/infra/typeorm/entities/Category';
 
 @injectable()
 class ListCategoriesUseCase {
   constructor(
     @inject('CategoriesRepository')
     private categoriesRepository: ICategoriesRepository
-  ) {}
+  ) { }
 
   async execute(): Promise<Category[]> {
     const categories = await this.categoriesRepository.list();
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,34 +1,3 @@
-import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
-import swaggerUI from 'swagger-ui-express';
-import { routes } from './routes';
-import swaggerFile from '../../../swagger.json';
-import createConnection from '@shared/infra/typeorm';
-import '@shared/container';
-import { AppError } from '@shared/errors/AppError';
-
-createConnection();
-
-const app = express();
-
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
-
-app.use(express.json());
-
-app.use(routes);
-
-app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
-  if (error instanceof AppError) {
-    return response.status(error.statusCode).json({
-      message: error.message
-    });
-  }
-
-  return response.status(500).json({
-    status: 'error',
-    message: `Internal Server Error - ${error.message}`
-  });
-});
+import { app } from './app';
 
 app.listen(3333, () => console.log('Server is running'));
