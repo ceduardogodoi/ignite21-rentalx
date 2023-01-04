152cf581027b51dc3208977df7618cf9a6895677
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-01-04 11:22:10 -0300

    Criando teste de listagem de categorias

--- src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -7,7 +7,7 @@ import createConnection from '@shared/infra/typeorm';
 
 let connection: Connection;
 
-describe('Create Category Controller', () => {
+describe('List Categories Controller', () => {
   beforeAll(async () => {
     connection = await createConnection();
     await connection.runMigrations();
@@ -26,7 +26,7 @@ describe('Create Category Controller', () => {
     await connection.close();
   });
 
-  it('should be able to create a new category', async () => {
+  it('should be able to list all categories', async () => {
     const responseToken = await request(app).post('/sessions').send({
       email: 'admin@rentx.com.br',
       password: 'admin'
@@ -34,7 +34,7 @@ describe('Create Category Controller', () => {
 
     const { token } = responseToken.body;
 
-    const response = await request(app)
+    await request(app)
       .post('/categories')
       .send({
         name: "Category Supertest",
@@ -44,27 +44,10 @@ describe('Create Category Controller', () => {
         Authorization: `Bearer ${token}`,
       });
 
-    expect(response.status).toBe(201);
-  });
-
-  it('should not be able to create a new category with same name', async () => {
-    const responseToken = await request(app).post('/sessions').send({
-      email: 'admin@rentx.com.br',
-      password: 'admin'
-    });
-
-    const { token } = responseToken.body;
-
     const response = await request(app)
-      .post('/categories')
-      .send({
-        name: "Category Supertest",
-        description: "Category Supertest"
-      })
-      .set({
-        Authorization: `Bearer ${token}`,
-      });
+      .get('/categories')
 
-    expect(response.status).toBe(400);
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveLength(1);
   });
 });
